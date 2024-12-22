48861b7ab5dd6fcef437d0d12d43f7c520cf9c24
rohitbisht0911@gmail.com <rohitbisht0911@gmail.com>
2024-12-22 16:10:40 +0530

    Initial commit

---
+++ src/main/java/com/HomeWorkWeekFour/CurrencyConvertor/demo/WeekFourHomeWorkOfCorrencyConvertorApplication.java
@@ -0,0 +1,13 @@
+package com.HomeWorkWeekFour.CurrencyConvertor.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeekFourHomeWorkOfCorrencyConvertorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeekFourHomeWorkOfCorrencyConvertorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/HomeWorkWeekFour/CurrencyConvertor/demo/WeekFourHomeWorkOfCorrencyConvertorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.HomeWorkWeekFour.CurrencyConvertor.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeekFourHomeWorkOfCorrencyConvertorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
